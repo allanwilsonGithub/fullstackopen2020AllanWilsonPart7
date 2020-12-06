bbb4ed0a760c79687be7ad2ba6444b08bf0d1cf0
Allan Wilson <allan@allanwilson.net>
2020-12-07 01:52:22 +0200

    Why does the button always activate on create?

--- src/App.js
+++ src/App.js
@@ -87,7 +87,9 @@ const CreateNew = (props) => {
           <input  {...info} />
         </div>
         <button>create</button>
+        <button type="button" onClick={console.log('1')}>Other Action</button>
       </form>
+
     </div>
   )
 
